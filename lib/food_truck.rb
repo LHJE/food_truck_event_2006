ef48e183eb3711dd85eaaf3525b00a34786bd470
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-05 09:21:02 -0600

    test_stock passing, will need updates

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class FoodTruck
 
   attr_reader :name, :inventory
@@ -8,13 +10,17 @@ class FoodTruck
 
   def check_stock(item)
     in_stock = 0
-    if inventory.any?(item) == false
+    if inventory.keys.any?(item) == false
       in_stock = 0
+    else
+      in_stock = inventory[item]
     end
     in_stock
   end
 
-
+  def stock(item, quantity)
+    inventory[item] = quantity
+  end
 
 
 
--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -33,7 +33,7 @@ class FoodTruckTest < Minitest::Test
     assert_equal 0, food_truck.check_stock(item1)
 
     food_truck.stock(item1, 30)
-    
+
     assert_equal ({item1 => 30}), food_truck.inventory
     assert_equal 30, food_truck.check_stock(item1)
   end
