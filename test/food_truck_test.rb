a80b120c7df8909601a530671bfebdd2edcd9ff7
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-08-05 09:08:29 -0600

    has_attributes test added to FoodTruckTest.

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -5,13 +5,17 @@ require './lib/food_truck'
 class FoodTruckTest < Minitest::Test
 
   def test_it_exists
-    food_truck = FoodTruck.new(name)
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
 
     assert_instance_of FoodTruck, food_truck
 
   end
 
+  def test_it_has_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
 
+    assert_equal "Rocky Mountain Pies", food_truck.name
+  end
 
 
 
